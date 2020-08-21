0aedea917d3ac19ef92d7fe5f841310e0f616c94
tmhho <hthiminhha@gmail.com>
2020-08-21 10:51:40 +0200

    update yri_afs

--- yri_afs.py
+++ yri_afs.py
@@ -8,10 +8,10 @@ lines = loadtxt("FoldSFS_YRI.txt", delimiter ="\t", unpack = False)
 afs = [line[1] for line in lines]
 # subsampled_yri_afs = afstools.subsample(afs,6)
 yri_afs = afstools.normalized(afs)[0]
-yri_afs = afstools.graphical_transform(yri_afs)
+graphically_transformed_yri_afs = afstools.graphical_transform(yri_afs)
 
 fig=go.Figure()
-fig = afstools.visualize_afs(afs= yri_afs, namefile = 'yri_afs',fig = fig, nameline = 'yri_afs')
+fig = afstools.visualize_afs(afs= graphically_transformed_yri_afs, namefile = 'yri_afs',fig = fig, nameline = 'yri_afs')
 
 
 num_islands = ['5'
@@ -31,12 +31,18 @@ migration_rates = ['0.1'
 # ,'10.0'
 ]
 
-data = afstools.simulated_nislands_size_inscreased_all_islands(num_islands=num_islands, list_T =list_T, list_x = list_x,migration_rates = migration_rates, nreps = '3000000')
+data = afstools.simulated_nislands_size_inscreased_all_islands(num_islands=num_islands, list_T =list_T, list_x = list_x,migration_rates = migration_rates, nreps = '3000')
 
-print(data.keys())
-afs = data['5islands_1T_0.1x_0.1M']
-afs = afstools.normalized(afs)[0]
-afs = afstools.graphical_transform(afs)
-# print(afs)
-fig = afstools.visualize_afs(afs= afs, namefile = 'yri_afs',fig = fig, nameline = 'nislands_size_inscreased_all_islands:5i_1T_0.1x_0.1M', show=True)
+keys = list(data.keys())
+dict_distances = {}
+for key in keys[1:]:
+	afs = data[key]
+	dict_distances[key] = afstools.average_absolute_error(yri_afs,afs)
+print(dict_distances)
+
+# afs = data['5islands_1T_0.1x_0.1M']
+# afs = afstools.normalized(afs)[0]
+# graphically_transformed_afs = afstools.graphical_transform(afs)
+# # print(afs)
+# fig = afstools.visualize_afs(afs= graphically_transformed_afs, namefile = 'yri_afs',fig = fig, nameline = 'nislands_size_inscreased_all_islands:5i_1T_0.1x_0.1M', show=True)
 
